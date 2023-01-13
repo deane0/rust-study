761c39d7e74d7a0ade06f757a1aaa649924552bc
Deane Leung <deane.leung@gmail.com>
2023-01-13 15:35:57 +0800

    :sparkles: Chapter 2: add example `Progress Reporting from Multiple Threads`

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,18 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let num_done = AtomicUsize::new(0);
-
-    let main_thread = thread::current();
+    let num_done = &AtomicUsize::new(0);
 
     thread::scope(|s| {
-        // A background thread to process all 100 times.
-        s.spawn(|| {
-            for i in 0..100 {
-                process_item(i); // Assuming this takes some time.
-                num_done.store(i + 1, Relaxed);
-                main_thread.unpark(); // Wake up the main thread.
-            }
-        });
+        // Four background threads to process all 100 items, 25 each.
+        for t in 0..4 {
+            s.spawn(move || {
+                for i in 0..25 {
+                    process_item(t * 25 + i); // Assuming this takes some time.
+                    num_done.fetch_add(1, Relaxed);
+                }
+            });
+        }
 
         // The main thread shows status updates, every second.
         loop {
@@ -24,16 +23,13 @@ fn main() {
             if n == 100 {
                 break;
             }
-
             println!("Working.. {n}/100 done");
-            thread::park_timeout(Duration::from_secs(1));
+            thread::sleep(Duration::from_secs(1));
         }
     });
-
-    println!("Done!");
 }
 
 fn process_item(_i: usize) {
-    thread::sleep(Duration::from_millis(1200));
-    println!("Processing item done.");
+    thread::sleep(Duration::from_millis(200));
+    println!("Processing item done!");
 }
